49366a7aa9f17c80c63c518fc0ab5d6e55ee9ced
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-06-19 00:51:37 +0530

    Add AllRoutes file to configure application routes

---
+++ backend/internals/routes/AllRoutes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/SAURABH-CHOUDHARI/memecity/pkg/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+// SetupRoutes configures all application routes
+func AllRoutes(app *fiber.App, conn storage.Repository) {
+    // Base API group
+	api := app.Group("/api")
+    
+    
+    UserRoutes(api, conn)
+
+}
