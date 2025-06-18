ec1e15f22640da5b75ec5d08eb4a22fae269024b
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-06-19 02:58:30 +0530

    Refactor Meme model to set default title and add missing fields for price, sale status, and relations

--- backend/internals/models/meme.go
+++ backend/internals/models/meme.go
@@ -9,10 +9,15 @@ import (
 
 type Meme struct {
 	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Title     string         `gorm:"not null"`
+	Title     string         `gorm:"default:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvzTAz0n_p5GyzLkmRP0JZ-r_Cd0dEfUmbjw&s'"`
 	ImageURL  string         `gorm:"type:text;not null"`
 	Tags      datatypes.JSON `gorm:"type:jsonb"`
-	Caption   string
-	OwnerID   uuid.UUID `gorm:"type:uuid;not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
+	Caption   string         `gorm:"not null"`	
+	Price     int            `gorm:"not null;default:0"`
+	OnSale    bool           `gorm:"default:false"`
+	OwnerID   uuid.UUID      `gorm:"type:uuid;not null;constraint:OnDelete:CASCADE"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	
+	// Relations
+	Owner User `gorm:"foreignKey:OwnerID;references:ID"`
 }
