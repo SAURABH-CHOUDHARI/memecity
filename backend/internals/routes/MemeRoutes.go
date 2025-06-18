c1a321a659e74fbb3ac57976709116c5541c6a38
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-06-19 02:58:08 +0530

    Add MemeRoutes for meme management with authentication

---
+++ backend/internals/routes/MemeRoutes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/SAURABH-CHOUDHARI/memecity/api/handlers"
+	"github.com/SAURABH-CHOUDHARI/memecity/internals/middlewares"
+	"github.com/SAURABH-CHOUDHARI/memecity/pkg/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+func MemeRoutes(router fiber.Router, conn storage.Repository){
+	auth:= router.Group("/auth", middlewares.AuthMiddleware(conn))
+
+	router.Get("/memes",handlers.CreateUser(conn))
+	auth.Post("/memes",handlers.CreateMeme(conn))
+	auth.Patch("/memes/:id/sale", handlers.ToggleMemeOnSale(conn))
+}
