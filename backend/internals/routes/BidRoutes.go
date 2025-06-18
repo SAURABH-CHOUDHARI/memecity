6d0e20bc98c954ba9a9bf782735898be9079a35b
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-06-19 03:43:56 +0530

    Add BidRoutes for handling bid submissions with authentication

---
+++ backend/internals/routes/BidRoutes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/SAURABH-CHOUDHARI/memecity/api/handlers"
+	"github.com/SAURABH-CHOUDHARI/memecity/internals/middlewares"
+	"github.com/SAURABH-CHOUDHARI/memecity/pkg/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+func BidRoutes(router fiber.Router, conn storage.Repository){
+	auth:= router.Group("/auth", middlewares.AuthMiddleware(conn))
+	
+	auth.Post("/bids",handlers.PlaceBid(conn))
+}
